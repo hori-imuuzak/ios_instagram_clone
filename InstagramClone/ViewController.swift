37530c2e018f6be36a54b98ba8cb68301aa1aa0d
堀知海 <kazuumi.hori@spacemarket.co.jp>
2020-05-06 22:42:49 +0900

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by 堀知海 on 2020/05/06.
+//  Copyright © 2020 umichan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
